ab715844603c2754571fe4decb22e7aba1655a37
Mahmoud Kharbat <mahmoodkharbat@gmail.com>
2022-08-29 17:57:15 +0300

    add motion to player using mouse and keyboard

--- hw1_315155770/Assets/Scripts/PlayerMotion.cs
+++ hw1_315155770/Assets/Scripts/PlayerMotion.cs
@@ -7,40 +7,35 @@ public class PlayerMotion : MonoBehaviour
     CharacterController cController;
     float rotationAroundY = 0;
     float rotationAroundX = 0;
-    float angularSpeed = 100f;
+    float angularSpeed = 5f;
     float horizontalInput;
     float speed=5f;
     public GameObject aCamera; // public means that it must be connected to some object in Unity
+
+
     // Start is called before the first frame update
     void Start()
     {
         cController = GetComponent<CharacterController>(); // connect to Character controller of player
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        /*float deltaz; 
+        float deltaz; 
 
-        rotationAroundX = -2*Input.GetAxis("Mouse Y") * angularSpeed * Time.deltaTime;
+        rotationAroundX = -Input.GetAxis("Mouse Y") * angularSpeed ;
         // rotates only camera
         aCamera.transform.Rotate(new Vector3(rotationAroundX, 0, 0));
-
-        // simple motion of player to z axis
-        // transform.position += new Vector3(0,0,-0.05f);
         
-        rotationAroundY = Input.GetAxis("Mouse X")*angularSpeed * Time.deltaTime;
+        rotationAroundY = Input.GetAxis("Mouse X")*angularSpeed;
         transform.Rotate(new Vector3(0, rotationAroundY, 0));
 
         // Time.deltaTime is time btween frames
         deltaz = speed*Input.GetAxis("Vertical")*Time.deltaTime; // can be {1,0,-1}
         
-        Vector3 motion = new Vector3(0, -0.5f, deltaz);// always forward in Local coordinates*/
-        Vector3 motion = new Vector3(0, -0.5f, -0.05f);
+        Vector3 motion = new Vector3(0, -0.5f, -deltaz);// always forward in Local coordinates
         motion = transform.TransformDirection(motion); // transforms motion to GLOBAL coordinates
         cController.Move(motion);// gets vector in GLOBAL coordinates
-
-       // GetComponent<Rigidbody>().velocity = new Vector3(horizontalInput, 0, 0);
     }
 }
