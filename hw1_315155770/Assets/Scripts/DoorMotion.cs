b5de73c0691adcaf69b74bbfd3f1e69cd061da9a
Mahmoud Kharbat <mahmoodkharbat@gmail.com>
2022-08-30 12:47:32 +0300

    adding audio to the door motion

--- hw1_315155770/Assets/Scripts/DoorMotion.cs
+++ hw1_315155770/Assets/Scripts/DoorMotion.cs
@@ -5,20 +5,25 @@ using UnityEngine;
 public class DoorMotion : MonoBehaviour
 {
     private Animator animator;
+    private AudioSource audioSrc;
+
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        audioSrc = GetComponent<AudioSource>();
     }
 
     private void OnTriggerEnter(Collider other)
     {
         animator.SetBool("isOpenning", true);
+        audioSrc.PlayDelayed(0.5f);
     }
 
     private void OnTriggerExit(Collider other)
     {
         animator.SetBool("isOpenning", false);
+        audioSrc.PlayDelayed(0.5f);
     }
     // Update is called once per frame
     void Update()
