a257123efcebe7eb058d9a5d3a5848d445e3e313
Mahmoud Kharbat <mahmoodkharbat@gmail.com>
2022-09-15 06:12:05 +0300

    finally animation switch works properly

--- hw1_315155770/Assets/Scripts/npcMotion.cs
+++ hw1_315155770/Assets/Scripts/npcMotion.cs
@@ -20,44 +20,25 @@ public class npcMotion : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        animator.SetBool("isMoving", true);
-        //animator.SetBool("isMoving", true);
-        /*time += Time.deltaTime;
-        print("the time is: " + time.ToString("integer"));
-        if (time % 5 == 0)
-        {
-            animator.SetBool("isMoving", true);
-        }
-        if (time % 2 == 0)
-        {
-            animator.SetBool("isMoving", false);
-        }*/
-
-        /*print("your place is: " + agent.transform.position);
-        if(agent.transform.position == new Vector3(-7, 3, -35) ||
-            agent.transform.position == new Vector3(-10, 3, -28)
-            )
-        {
-            animator.SetBool("isMoving", false);
-            yield return new WaitForSeconds(10f);
-        }*/
-        //animator.SetBool("isMoving", true);
         agent.SetDestination(target.transform.position);
-        StartCoroutine(changeState());
     }
 
-    IEnumerator changeState()
+    private void OnTriggerEnter(Collider other)
     {
-        print("hello your position is: " + agent.transform.position.ToString("F"));
-        if (true)
+        StartCoroutine(changeState(other));
+    }
+
+
+    IEnumerator changeState(Collider other)
+    {
+        animator.SetBool("isMoving", true);
+        if (target.gameObject == other.gameObject)
         {
-            print("hello2");
-            agent.SetDestination(agent.transform.position);
+            agent.enabled = false;
             animator.SetBool("isMoving", false);
             yield return new WaitForSeconds(5f);
             animator.SetBool("isMoving", true);
-            agent.SetDestination(target.transform.position);
-            print("hello3");
+            agent.enabled = true;
         }
             
     }
