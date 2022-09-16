12e0e3370680afb77e5b86187a89b38800976b55
Mahmoud Kharbat <mahmoodkharbat@gmail.com>
2022-09-16 23:35:19 +0300

    adding two characters, adding animation

---
+++ hw1_315155770/Assets/Scripts/npc2Motion.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class npc2Motion : MonoBehaviour
+{
+    private Animator animator;
+    private NavMeshAgent agent;
+    public GameObject target;
+    private float time;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+        agent.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (agent.enabled)
+            agent.SetDestination(target.transform.position);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        StartCoroutine(changeState(other));
+    }
+
+
+    IEnumerator changeState(Collider other)
+    {
+        if (target.gameObject == other.gameObject)
+        {
+            agent.enabled = false;
+            animator.SetInteger("npcState", 2);
+            yield return new WaitForSeconds(5f);
+            animator.SetInteger("npcState", 1);
+            agent.enabled = true;
+        }
+        else if (agent.transform.position.x > -9)
+        {
+            agent.enabled = false;
+            animator.SetInteger("npcState", 0);
+            yield return new WaitForSeconds(10f);
+            animator.SetInteger("npcState", 1);
+            agent.enabled = true;
+        }
+
+    }
+}
